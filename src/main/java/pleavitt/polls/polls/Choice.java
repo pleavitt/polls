dc0d2b4cfa1baf8d527858fbf3a249e2d3c521b8
pleavitt <pleavitt@gmail.com>
2018-08-14 20:45:23 +1000

    Initial Commit

--- src/main/java/pleavitt/polls/polls/Choice.java
+++ src/main/java/pleavitt/polls/polls/Choice.java
@@ -1,10 +1,19 @@
 package pleavitt.polls.polls;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
 @Getter
 @Setter
+@ToString
+@NoArgsConstructor
 public class Choice {
     private String choice;
     private int votes;
--- src/main/java/pleavitt/polls/polls/Question.java
+++ src/main/java/pleavitt/polls/polls/Question.java
@@ -1,12 +1,20 @@
 package pleavitt.polls.polls;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.List;
 
+@Entity
+@Table
 @Getter
 @Setter
+@ToString
+@NoArgsConstructor
 public class Question {
 
     String question;
